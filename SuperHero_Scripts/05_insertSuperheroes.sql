4fbbba1bbff1bbff2d299211bb44b0670a920166
langheim <92371180+langheim@users.noreply.github.com>
2021-11-22 21:56:54 +0100

    Add files via upload

---
+++ SuperHero_Scripts/05_insertSuperheroes.sql
@@ -0,0 +1,15 @@
+/*Insert some SuperHeros*/
+INSERT INTO Superhero (nameSuperhero, aliasSuperhero, originSuperhero)
+VALUES ('Henry Oym','Ant-Man','Atlanta')
+
+INSERT INTO Superhero (nameSuperhero, aliasSuperhero, originSuperhero)
+VALUES ('Arthur Curry','Aquaman','Atlantis')
+
+INSERT INTO Superhero (nameSuperhero, aliasSuperhero, originSuperhero)
+VALUES ('Clint Barton','Hawkeye','Missouri')
+
+INSERT INTO Superhero (nameSuperhero, aliasSuperhero, originSuperhero)
+VALUES ('Arno Stark','Iron Man','New York')
+
+INSERT INTO Superhero (nameSuperhero, aliasSuperhero, originSuperhero)
+VALUES ('James Howlett','Wolverine','Canada')
---
+++ SuperHero_Scripts/08_updateSuperhero.sql
@@ -0,0 +1,9 @@
+/*Update SuperHero*/
+UPDATE Superhero
+SET nameSuperhero = 'Iron Man', aliasSuperhero = 'Arno Stark'
+WHERE aliasSuperhero = 'Iron Man'
+
+UPDATE Superhero
+SET nameSuperhero = 'Aquaman', aliasSuperhero = 'Arthur Curry'
+WHERE aliasSuperhero = 'Aquaman'
+
